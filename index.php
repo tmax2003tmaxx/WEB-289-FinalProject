2e7805797997e2dc876f81c9369484e23fa67c9b
JeffMatson <jeff@jeffmatson.net>
2016-05-08 20:40:57 -0400

    Corrected spacing.  You should probably use an autoloader here.

--- index.php
+++ index.php
@@ -19,24 +19,27 @@ if ($action === NULL) {
 $lName = '';
 $users = array();
 
+/**
+ * An autoloader would be more appropriate here
+ * @see spl_autoload_register()
+ */
+
 switch( $action ) {
-        case 'home':
+    case 'home':
         include_once ('view/home.php');
-
-break;
-        case 'gallery':
+    break;
+    case 'gallery':
         include_once ('view/gallery.php');
-
-break; 
-        case 'login':
+    break;
+    case 'login':
         include_once ('view/bmdLogin.php');
-
-break;
-        case 'services':
+    break;
+    case 'services':
         include_once ('view/services.php');
+    break;
 
-break;
-        case 'buyHappiness':
+    case 'buyHappiness':
+
         if (isset($_SESSION["f_name"]) && ($_SESSION["userLevel"] == 'Admin')){
             include_once ('view/buyHappinessAdmin.php'); 
         } elseif (isset($_SESSION["f_name"]) && ($_SESSION["userLevel"] == 'Member')){ 
@@ -44,42 +47,41 @@ break;
         } else {
             include_once('view/buyHappiness.php');
         }
-break;
-        case 'user_dashboard':
-        include_once ('view/user_dashboard.php');
-break;
-        case 'logout':
-        include_once ('view/logout.php');
-break;
-        case 'member_dashboard':
-        include_once ('view/member_dashboard.php');
-break;
-        case 'contact':
-        include_once ('view/contact.php');  
-break;
-        case 'signUp':
-        include_once ('view/register_customer.php');
-break;
-        case 'add_user':
-        
-    // Add User
-   
-    
-    $fName = filter_input(INPUT_POST, 'f_name');
-    $lName = filter_input(INPUT_POST, 'l_name');
-    $email = filter_input(INPUT_POST, 'email');
-    $address = filter_input(INPUT_POST, 'address');
-    $userLevel = filter_input(INPUT_POST, 'userLevel');
-    $password = filter_input(INPUT_POST, 'password');
-    $phone = filter_input(INPUT_POST, 'phone');
-    
-add_user($fName, $lName, $email, $address, $userLevel, $password, $phone);
-    // // Display the user list
 
-    include_once ('view/bmdLogin.php');
-break;
+    break;
+
+    case 'user_dashboard':
+        include_once ('view/user_dashboard.php');
+    break;
+    case 'logout':
+        include_once ('view/logout.php');
+    break;
+    case 'member_dashboard':
+        include_once ('view/member_dashboard.php');
+    break;
+    case 'contact':
+        include_once ('view/contact.php');
+    break;
+    case 'signUp':
+        include_once ('view/register_customer.php');
+    break;
+    case 'add_user':
+        
+        // Add User
+        $fName     = filter_input(INPUT_POST, 'f_name');
+        $lName     = filter_input(INPUT_POST, 'l_name');
+        $email     = filter_input(INPUT_POST, 'email');
+        $address   = filter_input(INPUT_POST, 'address');
+        $userLevel = filter_input(INPUT_POST, 'userLevel');
+        $password  = filter_input(INPUT_POST, 'password');
+        $phone     = filter_input(INPUT_POST, 'phone');
+
+        add_user($fName, $lName, $email, $address, $userLevel, $password, $phone);
+
+        // Display the user list
+        include_once ('view/bmdLogin.php');
+
+    break;
 }
 
-
 include 'view/footer.php';
-?>
